9c5abd8263acaf4e5f98ec6eb4a74a1b0dec9ea0
Artem Markelov <artem77timoha77@gmail.com>
2022-11-05 12:39:14 +0600

    Made a menu component. Which displays the positions of the goods on the screen with their data.

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import {MenuData} from "../../types";
+import Menu from  "../../components/Menu/Menu"
+
+const menuArray:MenuData[] = [
+  {name: 'Hamburger', price: 80, mount: 0},
+  {name: 'Cheeseburger', price: 90, mount: 0},
+  {name: 'Fries', price: 45, mount: 0},
+  {name: 'Coffee', price: 70, mount: 0},
+  {name: 'Tea', price: 50, mount: 0},
+  {name: 'Cola', price: 40, mount: 0},
+]
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className='order-details'>
+
+
+      </div>
+
+
+
+
+      <div className='add-items'>
+        <Menu menuArray={menuArray}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
