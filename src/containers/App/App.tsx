cf6bf3043049845f1a03888d79ac05b3118ba8c0
Artem Markelov <artem77timoha77@gmail.com>
2022-11-05 17:53:29 +0600

    Added a card component that will insert cards into the menu component. And I added another component that displays the total cost of the order. Check that when there is no order, it displays a message. Refactored the code and added styles.

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,45 +1,47 @@
 import React, {useState} from 'react';
-import './App.css';
-import Menu from  "../../components/Menu/Menu"
+import Menu from "../../components/Menu/Menu"
 import OrderDetails from "../../components/OrderDetails/OrderDetails";
+import hamburgerImage from '../../assets/images/hamburger.png';
+import cheeseBurgerImage from '../../assets/images/cheeseburger.png';
+import teaImage from '../../assets/images/tea.png';
+import coffeeImage from '../../assets/images/coffee.png';
+import friesImage from '../../assets/images/fries.png';
+import colaImage from '../../assets/images/cola.png';
+import './App.css';
 
 function App() {
 
   const [menuArray, setMenuArray] = useState([
-    {name: 'Hamburger', price: 80, mount: 0},
-    {name: 'Cheeseburger', price: 90, mount: 0},
-    {name: 'Fries', price: 45, mount: 0},
-    {name: 'Coffee', price: 70, mount: 0},
-    {name: 'Tea', price: 50, mount: 0},
-    {name: 'Cola', price: 40, mount: 0},
+    {name: 'Hamburger', price: 80, mount: 0, image: hamburgerImage},
+    {name: 'Cheeseburger', price: 90, mount: 0, image: cheeseBurgerImage},
+    {name: 'Fries', price: 45, mount: 0, image: friesImage},
+    {name: 'Coffee', price: 70, mount: 0, image: coffeeImage},
+    {name: 'Tea', price: 50, mount: 0, image: teaImage},
+    {name: 'Cola', price: 40, mount: 0, image: colaImage},
   ])
 
-  const getMount = (name:string)=> {
-    setMenuArray(prev => prev.map((item)=>{
+  const getMount = (name: string) => {
+    setMenuArray(prev => prev.map((item) => {
       return item.name === name ? {
         ...item,
         mount: item.mount + 1,
-      } : item
+      } : item;
     }))
-}
+  }
 
-const btnDelete = (name:string) =>{
-    setMenuArray(prev => prev.map((item)=>{
+  const btnDelete = (name: string) => {
+    setMenuArray(prev => prev.map((item) => {
       return item.name === name ? {
         ...item,
         mount: 0,
-      } : item
+      } : item;
     }))
-}
+  }
 
   return (
     <div className="App">
-      <div className='order-details'>
-        <OrderDetails menuArray={menuArray} btnDelete={btnDelete}/>
-      </div>
-      <div className='add-items'>
-        <Menu menuArray={menuArray} btnDelete={getMount}/>
-      </div>
+      <OrderDetails menuArray={menuArray} btnDelete={btnDelete}/>
+      <Menu menuArray={menuArray} btnDelete={getMount}/>
     </div>
   );
 }
