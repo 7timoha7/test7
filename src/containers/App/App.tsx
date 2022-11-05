f68f407c06f4a2a88966ef4f20c2da2c16f29d25
Artem Markelov <artem77timoha77@gmail.com>
2022-11-05 13:53:04 +0600

    I made the second component, where order items are displayed and their cost is calculated. And the button on which the position is deleted.

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {MenuData} from "../../types";
 import Menu from  "../../components/Menu/Menu"
-
-const menuArray:MenuData[] = [
-  {name: 'Hamburger', price: 80, mount: 0},
-  {name: 'Cheeseburger', price: 90, mount: 0},
-  {name: 'Fries', price: 45, mount: 0},
-  {name: 'Coffee', price: 70, mount: 0},
-  {name: 'Tea', price: 50, mount: 0},
-  {name: 'Cola', price: 40, mount: 0},
-]
+import OrderDetails from "../../components/OrderDetails/OrderDetails";
 
 function App() {
+
+  const [menuArray, setMenuArray] = useState([
+    {name: 'Hamburger', price: 80, mount: 0},
+    {name: 'Cheeseburger', price: 90, mount: 0},
+    {name: 'Fries', price: 45, mount: 0},
+    {name: 'Coffee', price: 70, mount: 0},
+    {name: 'Tea', price: 50, mount: 0},
+    {name: 'Cola', price: 40, mount: 0},
+  ])
+
+  const getMount = (name:string)=> {
+    setMenuArray(prev => prev.map((item)=>{
+      return item.name === name ? {
+        ...item,
+        mount: item.mount + 1,
+      } : item
+    }))
+}
+
+const btnDelete = (name:string) =>{
+    setMenuArray(prev => prev.map((item)=>{
+      return item.name === name ? {
+        ...item,
+        mount: 0,
+      } : item
+    }))
+}
+
   return (
     <div className="App">
-
       <div className='order-details'>
-
-
+        <OrderDetails menuArray={menuArray} btnDelete={btnDelete}/>
       </div>
-
-
-
-
       <div className='add-items'>
-        <Menu menuArray={menuArray}/>
+        <Menu menuArray={menuArray} btnDelete={getMount}/>
       </div>
     </div>
   );
